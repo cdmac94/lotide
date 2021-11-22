3780f80a35a3d54f1c05429fa2388227b7173bdb
cdmac94 <collinmacleod79@gmail.com>
2021-11-22 23:08:53 +0000

    cleaning up with Template Literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Asertation Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Asertation Passed: ${actual}  ===   ${expected}`);
   } else {
-    console.log("🛑🛑🛑 Assertation Failed: " + actual + " !== " + expected);
+    console.log(`🛑🛑🛑 Assertation Failed: ${actual} !== ${expected}`);
   }
 };
 
