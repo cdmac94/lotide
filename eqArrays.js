ae4d87eb4718ec4e520ea7be06436dc8d093549c
cdmac94 <collinmacleod79@gmail.com>
2021-11-24 07:46:05 +0000

    Lint used

--- eqArrays.js
+++ eqArrays.js
@@ -6,22 +6,21 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(arrayOne, arrayTwo){
+const eqArrays = function(arrayOne, arrayTwo) {
   
   if (arrayOne.length !== arrayTwo.length) {
     return false;
-  }     
-  for (let x = 0, i = 0; i < arrayOne.length; x++, i++){     
-      if (arrayOne[x] === arrayTwo[i]){
-        continue;
-      } else {
-        return false
-      }
-    } return true
-} 
+  }
+  for (let x = 0, i = 0; i < arrayOne.length; x++, i++) {
+    if (arrayOne[x] === arrayTwo[i]) {
+      continue;
+    } else {
+      return false;
+    }
+  } return true;
+};
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true ) 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true ) 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false ) 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
