98cb4bd4c2f5647d532673767a3c153d45ef2e6e
cdmac94 <collinmacleod79@gmail.com>
2021-11-24 15:20:07 -0800

    Create countLetters.js
    Counting letters in string

---
+++ countLetters.js
@@ -0,0 +1,10 @@
+const countLetters = function(text) {
+  const count = {};
+  for (let i = 0; i < text.length; i++) {
+    count[text.charAt(i)] = text.split(text.charAt(i)).length - 1 
+  }
+  console.log(count)
+  return count
+  }
+
+  countLetters("Lighthouse");
