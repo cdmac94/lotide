b2901cbf2b3159c2922bd345029d0c33a52ee7a4
cdmac94 <collinmacleod79@gmail.com>
2021-11-24 07:29:26 +0000

    Function for comparing arrays dirctly

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+const assertArraysEqual = function(arrayOne, arrayTwo){
+  
+  let result = false
+
+  if (arrayOne.length !== arrayTwo.length) {
+    result = false;
+  }     
+  for (let x = 0, i = 0; i < arrayOne.length; x++, i++){     
+      if (arrayOne[x] === arrayTwo[i]){
+      result = true;
+      } else {
+        result = false
+      }
+    }
+
+    if (result === true) {
+      console.log(`✅✅✅ Asertation Passed: ${arrayOne}  ===   ${arrayTwo}`);
+    } else {
+      console.log(`🛑🛑🛑 Assertation Failed: ${arrayOne} !== ${arrayTwo}`);
+    }
+
+  };
+
+
+
+assertArraysEqual(["1", "2", "3", 3], ["1", "2", "3"])
