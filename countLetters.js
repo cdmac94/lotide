264cdab9587dbbac68da2a1765879a6f05bbd66f
cdmac94 <collinmacleod79@gmail.com>
2021-12-01 08:48:48 -0800

    Update countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -1,10 +1,11 @@
 const countLetters = function(text) {
   const count = {};
   for (let i = 0; i < text.length; i++) {
-    count[text.charAt(i)] = text.split(text.charAt(i)).length - 1 
-  }
-  console.log(count)
-  return count
-  }
 
-  countLetters("Lighthouse");
+    count[text.charAt(i)] = text.split(text.charAt(i)).length - 1;
+  }
+  console.log(count);
+  return count;
+};
+
+module.exports = countLetters;
