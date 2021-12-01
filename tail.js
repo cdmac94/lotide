b4a2b1b3976baaf8d6b4e18ae101d156bec1f3cf
cdmac94 <collinmacleod79@gmail.com>
2021-12-01 08:57:09 -0800

    Update tail.js

--- tail.js
+++ tail.js
@@ -1,20 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Asertation Passed: ${actual}  ===   ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertation Failed: ${actual} !== ${expected}`);
-  }
-};
+module.exports = tail;
 
 const tail = function(array) {
   var newArray = [array];
   newArray.shift();
   return newArray;
 };
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
