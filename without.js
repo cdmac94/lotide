d8d95c80013575da28bdf7e2c8c1ffae6d3ed263
cdmac94 <collinmacleod79@gmail.com>
2021-12-01 08:58:15 -0800

    Update without.js

--- without.js
+++ without.js
@@ -1,38 +1,11 @@
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  
-  let result = false;
+module.exports = tail;
 
-  if (arrayOne.length !== arrayTwo.length) {
-    result = false;
-  }
-  for (let x = 0, i = 0; i < arrayOne.length; x++, i++) {     
-      if (arrayOne[x] === arrayTwo[i]){
-      result = true;
-    } else {
-      result = false;
-      }
-  }
-
-  if (result === true) {
-    console.log(`✅✅✅ Asertation Passed: ${arrayOne}  ===   ${arrayTwo}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertation Failed: ${arrayOne} !== ${arrayTwo}`);
-  }
-
+const tail = function(array) {
+  var newArray = [array];
+  newArray.shift();
+  return newArray;
 };
 
-
-const without = function(source, itemsToRemove) {
-  let newArray = [];
-
-  for (let k = 0; k < source.length; k++) {
-    if (!itemsToRemove.includes(source[k])) {
-      newArray.push(source[k]);
-    }
-  } console.log(newArray);
-};
-
-without([1, 2, 3], [1]);
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]);
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
